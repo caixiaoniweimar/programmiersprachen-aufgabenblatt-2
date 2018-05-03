e52dae23cc86715e54a90ae9617e60078709b0a9
caixiaoniweimar <710389740@qq.com>
2018-05-03 18:00:15 +0200

    Add Rectangle

---
+++ source/rectangle.cpp
@@ -0,0 +1,34 @@
+#include "rectangle.hpp"
+
+Rectangle::Rectangle():
+    max_{ 1.0, 1.0 },
+    min_{ 0.0, 0.0 } {}
+
+Rectangle::Rectangle(Rectangle const& r):
+	max_{r.max_},
+	min_{r.min_} {}
+
+// Eigenschaft: Length+Height
+    void Rectangle::set_Min(Vec2 const& min){
+    	min_.x=min.x;
+    	min_.y=min.y;
+    }
+    double Rectangle::get_Length() const{
+    	return max_.x - min_.x;
+    }
+    void Rectangle::set_Max(Vec2 const& max){
+    	max_.x=max.x;
+    	max_.y=max.y;
+    }
+    double Rectangle::get_Height() const{
+    	return max_.y - min_.y;
+    }
+// Eigenschaft: Area/Flaeche
+    double Rectangle::area_rectangle() const{
+    	return (max_.x - min_.x)*(max_.y - min_.y);
+    }
+// Eigenschaft: circumference
+    double Rectangle::circumference() const{
+    	return 2*(max_.x - min_.x)+2*(max_.y - min_.y);
+    }
+Rectangle::~Rectangle() {}
---
+++ source/rectangle.hpp
@@ -0,0 +1,31 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "vec2.hpp"
+
+class Rectangle
+{
+public:
+	Rectangle();					//StandardKonstruktor
+	Rectangle(Rectangle const& r);  //Konstruktor
+
+    // Eigenschaft: Length+Height
+    void set_Min(Vec2 const& min);
+    double get_Length() const; 
+
+    void set_Max(Vec2 const& max);
+    double get_Height() const;
+
+    // Eigenschaft: Area/Flaeche
+    double area_rectangle() const;
+  
+    // Eigenschaft: circumference
+    double circumference() const;
+    
+	~Rectangle();
+
+private:
+	Vec2 max_;
+	Vec2 min_;
+
+};
+#endif
